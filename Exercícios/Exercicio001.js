60c380c1296175fa1b1db33ee309a550751db25b
fabiosabatinebock <fabiosabatinejuina@gmail.com>
2024-06-13 23:03:35 -0400

    Exercício e aula de função

---
+++ Exercícios/Exercicio001.js
@@ -0,0 +1,19 @@
+function SomaDoisValores(a, b){
+    return (a + b)
+}
+
+console.log(SomaDoisValores(5, 6))
+
+
+function SubtrairDoisValores(a, b){
+    return (a - b)
+}
+
+console.log(SubtrairDoisValores(10, 6))
+
+function MultiplicacaoDoisValores(a, b){
+    return (a * b)
+}
+
+console.log(MultiplicacaoDoisValores(8, 6))
+
