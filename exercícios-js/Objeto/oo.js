03c91ea436902dc8ce2da532f21e3db3a36f25a0
fabiosabatinebock <fabiosabatinejuina@gmail.com>
2024-06-19 22:39:17 -0400

    Aula de OO

---
+++ exercícios-js/Objeto/oo.js
@@ -0,0 +1,22 @@
+//Código não é executável!!!
+
+//Procedural
+processamento(valor1, valor2, valor3)
+
+//OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        //...
+    }
+}
+
+objeto.processamento()
+
+//Principios importantes:
+//Abstração
+//Encapsulamento
+//Herança
+//Polimorfismo
